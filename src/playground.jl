29b74f4a906d1a4c6341ef34dc0751c5b3413ef4
Alexandre Bergel <alexandre.bergel@me.com>
2021-11-03 12:29:27 -0300

    Added several examples

--- src/playground.jl
+++ src/playground.jl
@@ -23,7 +23,23 @@ function example02()
     rshow(canvas)
 end
 
-example02()
+function example03() 
+    canvas = RCanvas()
+    Random.seed!(42)
+    for x in 1:3
+        for y in 1:3
+            box = RBox(color=RColor(rand(), rand(), rand()))
+            translateTo!(box, x * 20, y * 20)
+            add!(canvas, box)
+        end
+    end
+    addCallback!(shapesOf(canvas), Callback(:mouseClick, (event, shape) -> removeShape!(canvas, shape)))
+    #addCallback!(firstBox, Callback(:mouseMove, (event, shape) -> println("CALLBACK mouse move $(shape)")))
+    rshow(canvas)
+end
+
+example03()
 
 print("Press Enter to exit")
 readline()
+
