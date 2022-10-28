ea9a51c65c2115fb8616b79145dc8f20de33e622
Alexandre Bergel <alexandre.bergel@relational.ai>
2022-10-28 18:14:52 +0200

    Example about

---
+++ src/random_playground.jl
@@ -0,0 +1,220 @@
+using Roassal
+
+my_log = "DEB: Rule=failure_RelVar rewrite_failed=true allocs=286160 Task=28573888619704622933
+DEB: Rule=bindings_pushdown_domain rewrite_failed=false allocs=61632 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=appl_sourceid_refine_shape_from_args rewrite_failed=false allocs=119884 Task=28573888619704622933
+DEB: Rule=failure_RelVar rewrite_failed=true allocs=26016 Task=28573888619704622933
+DEB: Rule=failure_Appl rewrite_failed=true allocs=2224 Task=28573888619704622933
+DEB: Rule=formula_sourceid_appl_to_core_appl rewrite_failed=false allocs=80 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplSource rewrite_failed=true allocs=15552 Task=28573888619704622933
+DEB: Rule=failure_Bindings rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=inline_relvar_from_attribute rewrite_failed=false allocs=28384 Task=28573888619704622933
+DEB: Rule=inline_relvar_from_attribute rewrite_failed=false allocs=20008 Task=28573888619704622933
+DEB: Rule=failure_RelVar rewrite_failed=true allocs=1200 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_Appl rewrite_failed=true allocs=3152 Task=28573888619704622933
+DEB: Rule=relabs_eta rewrite_failed=false allocs=6792 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_Appl rewrite_failed=true allocs=3200 Task=28573888619704622933
+DEB: Rule=formula_appl_to_core_appl rewrite_failed=false allocs=32 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6560 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=appl_scalarvar_beta rewrite_failed=false allocs=8640 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6576 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=appl_scalarvar_beta rewrite_failed=false allocs=8416 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6592 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=appl_corevar_beta rewrite_failed=false allocs=8064 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6608 Task=28573888619704622933
+DEB: Rule=relabs_no_bindings rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=appl_no_args rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=appl_scalarvar_beta rewrite_failed=false allocs=10448 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6448 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=appl_scalarvar_beta rewrite_failed=false allocs=7904 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6464 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=appl_no_args rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=relabs_bindings rewrite_failed=false allocs=83794 Task=28573888619704622933
+DEB: Rule=bindings_to_core rewrite_failed=false allocs=144 Task=28573888619704622933
+DEB: Rule=suchthat_relabs rewrite_failed=false allocs=80 Task=28573888619704622933
+DEB: Rule=suchthat_formula rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_And rewrite_failed=true allocs=960 Task=28573888619704622933
+DEB: Rule=relabs_no_bindings rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=exists_formula rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=relabs_flatten rewrite_failed=false allocs=831738 Task=28573888619704622933
+DEB: Rule=failure_RelAbstract rewrite_failed=true allocs=192 Task=28573888619704622933
+DEB: Rule=def_pointwise rewrite_failed=false allocs=6064 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2704 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_RelVar rewrite_failed=true allocs=1264 Task=28573888619704622933
+DEB: Rule=def_pointwise rewrite_failed=false allocs=2648 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplOther rewrite_failed=true allocs=2160 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2216 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=6464 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplOther rewrite_failed=true allocs=2160 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2200 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=core_appl_relation_lit_to_core rewrite_failed=false allocs=5944 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=appl_constant rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_RelVar rewrite_failed=true allocs=1184 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=arity0_appl_to_core_appl rewrite_failed=false allocs=272 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=656 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=appl_constant rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=arity0_appl_to_core_appl rewrite_failed=false allocs=272 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=656 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=appl_constant rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=arity0_appl_to_core_appl rewrite_failed=false allocs=272 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=656 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=appl_constant rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=arity0_appl_to_core_appl rewrite_failed=false allocs=272 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=656 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_Or rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=appl_core_corevar_beta rewrite_failed=false allocs=17632 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=320 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=688 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=320 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=688 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=320 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=688 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=320 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=688 Task=28573888619704622933
+DEB: Rule=failure_Or rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=core_relabs_no_bindings rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_Or rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=appl_no_args rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_Or rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=formula_flatten rewrite_failed=false allocs=0 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=304 Task=28573888619704622933
+DEB: Rule=failure_Or rewrite_failed=true allocs=128 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=3320 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplSource rewrite_failed=true allocs=583256 Task=28573888619704622933
+DEB: Rule=failure_And rewrite_failed=true allocs=960 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2704 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplNative rewrite_failed=true allocs=50016 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplSource rewrite_failed=true allocs=47784 Task=28573888619704622933
+DEB: Rule=failure_And rewrite_failed=true allocs=960 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2704 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=def_pointwise rewrite_failed=false allocs=1480 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=appl_constant rewrite_failed=false allocs=416 Task=28573888619704622933
+DEB: Rule=failure_RelVar rewrite_failed=true allocs=1200 Task=28573888619704622933
+DEB: Rule=failure_Appl rewrite_failed=true allocs=1872 Task=28573888619704622933
+DEB: Rule=formula_appl_to_core_appl rewrite_failed=false allocs=32 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=608 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2280 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=224 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=608 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2264 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933
+DEB: Rule=failure_ScalarVar rewrite_failed=true allocs=288 Task=28573888619704622933
+DEB: Rule=failure_TagCoreApplEqual rewrite_failed=true allocs=688 Task=28573888619704622933
+DEB: Rule=failure_CoreRelAbstract rewrite_failed=true allocs=0 Task=28573888619704622933
+DEB: Rule=failure_Def rewrite_failed=true allocs=2264 Task=28573888619704622933
+DEB: Rule=def_intro_from_outlined rewrite_failed=true allocs=80 Task=28573888619704622933"
+
+struct RuleApplication
+    name::String
+    rewrite_failed::Bool
+    allocs::Int
+    task_id::Int128
+end
+
+
+# Extracting data
+rule_per_line = split(my_log, '\n')
+rule_applications = RuleApplication[]
+for l in rule_per_line
+    ss = split(l)
+    name = split(ss[2], '=')[2]
+    rewrite_failed = split(ss[3], '=')[2] == "true"
+    allocs = parse(Int, split(ss[4], '=')[2])
+    task_id = parse(Int128, split(ss[5], '=')[2])
+    push!(rule_applications, RuleApplication(name, rewrite_failed, allocs, task_id))
+end
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Visualization
+
+function show_rule_applications(rule_applications::Vector{RuleApplication})
+    shape_size = 10
+    all_rules_names = unique(map(r -> r.name, rule_applications))
+
+    c = RCanvas()
+
+    for (index, rule_app) in enumerate(rule_applications)
+        box = RBox()
+        add!(c, box)
+        y = findall(n -> n == rule_app.name, all_rules_names)[1] * shape_size
+        translate_to!(box, index * shape_size, y)
+    end
+
+    rshow(c)
+end
+
+show_rule_applications(rule_applications)
