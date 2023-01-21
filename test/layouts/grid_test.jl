805ca00a68cf5f9cd0511fc8a044474f4819c063
Alexandre Bergel <alexandre.bergel@relational.ai>
2023-01-21 22:18:40 +0100

    Grid testing

--- test/layouts/grid_test.jl
+++ test/layouts/grid_test.jl
@@ -1,18 +1,22 @@
 
-@testset "Simple 01" begin
+@testset "Example 01" begin
     c = RCanvas()
 
-    for _ in 1:4
+    for _ in 1:10
         add!(c, RBox())
     end
-
-    boxes = get_shapes(c)
-    @test length(boxes) == 4
-    @test all(b -> pos(b) == (0, 0), boxes)
-
     apply(GridLayout(), c)
-    @test pos(boxes[1]) == (5.0, 5.0)
-    @test all(b -> pos(b) != (0, 0), boxes[2:end])
+    @test map(pos, get_shapes(c)) == [(5.0, 5.0), (20.0, 5.0), (35.0, 5.0), (50.0, 5.0), (5.0, 20.0), (20.0, 20.0), (35.0, 20.0), (50.0, 20.0), (5.0, 35.0), (20.0, 35.0)]
+end
+
+@testset "Example 02" begin
+    c = RCanvas()
+
+    for i in 1:10
+        add!(c, RBox(; size = i * 10, color=RColor(i/10, i/10, i/10)))
+    end
+    apply(GridLayout(), c)
+    @test map(pos, get_shapes(c)) == [(5.0, 5.0), (25.0, 10.0), (55.0, 15.0), (95.0, 20.0), (25.0, 70.0), (85.0, 75.0), (155.0, 80.0), (235.0, 85.0), (45.0, 175.0), (145.0, 180.0)]
 end
 
 @testset "Line count" begin
